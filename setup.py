dae0044d227a5614b6b44eec7871cac27105ff44
AlloteSoftware <allote.software@gmail.com>
2025-05-28 15:51:24 +0300

    Change version to 0.0.3

--- setup.py
+++ setup.py
@@ -9,11 +9,11 @@ with open(os.path.join(BASEDIR, 'README.rst'), 'r') as f:
 
 setup(
     name='mathml2omml',
-    version='0.0.3a1',
+    version='0.0.3',
     description='',
     long_description=LONG_DESCRIPTION,
-    url='https://github.com/amedama41/mathml2omml',
-    author='amedama41',
+    url='https://github.com/AlloteSoftware/mathml2omml_as',
+    author='amedama41(patched by Allote.Software)',
     author_email='kamo.devel41@gmail.com',
     keywords=['MathML', 'OpenXML'],
     packages=['mathml2omml'],
